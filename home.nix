ed0e6c98ea7ff3907e883dd0c6fbe50830ecbe3e
Nta1e <shadikntale@gmail.com>
2026-09-17 00:10:41 +0300

    🔧 Add herdr config

--- home.nix
+++ home.nix
@@ -375,6 +375,8 @@ in
   # Edit-in-place: the real file stays in my repo, ~/.config just points at it.
   home.file.".config/wezterm".source =
     config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/wezterm";
+  home.file.".config/herdr".source =
+    config.lib.file.mkOutOfStoreSymlink "${dotfiles}/home/.config/herdr";
 
 
   home.file.".pi/agent/themes".source =
